97185c2adcc0b35131b6663faf94cbf9de01e253
oa57 <ashburn.oscar@gmail.com>
2024-05-20 13:47:25 +1200

    formatting

--- Encrypt.py
+++ Encrypt.py
@@ -3,50 +3,50 @@ Encrypts .bnp images and outputs image as .jpeg
 with encrypted image bytes being the body
 """
 
+# initilization vector
+__iv__ = get_random_bytes(16)
+# 128byte key
+__hexkey__ = b"770A8A65DA156D24EE2A093277530142"
+
+
 from Crypto.Cipher import AES
 from Crypto.Util import Padding
 from PIL import Image
 from Crypto.Random import get_random_bytes
 from io import BytesIO
 
-#initilization vector
-__iv__ = get_random_bytes(16)
-
-#128byte key
-__hexkey__ = b"770A8A65DA156D24EE2A093277530142"
-
 
 def ECB_Encrypt(image_bytes):
     """
-    AES -ECB Encryption Mode 
+    AES -ECB Encryption Mode
 
     @param image_bytes Bytes to encrypt
 
     @return Return image bytes as ciphertext
     """
 
-    #set cipher mode
-    ECB_cypher =  AES.new(__hexkey__, AES.MODE_ECB)
-    #pad bytes
-    padded = Padding.pad(image_bytes,16)
-    #encrypt
+    # set cipher mode
+    ECB_cypher = AES.new(__hexkey__, AES.MODE_ECB)
+    # pad bytes
+    padded = Padding.pad(image_bytes, 16)
+    # encrypt
     ciphertext = ECB_cypher.encrypt(padded)
     return ciphertext
 
 
 def CBC_Encrypt(image_bytes):
     """
-    AES -CBC Encryption Mode 
+    AES -CBC Encryption Mode
 
     @param image_bytes Bytes to encrypt
 
     @return Return image bytes as ciphertext
     """
-        
-    #set cipher mode
-    CFB_cypher =  AES.new(__iv__, AES.MODE_CBC)
-    #pad bytes
-    ct_bytes = Padding.pad(image_bytes,16)
+
+    # set cipher mode
+    CFB_cypher = AES.new(__iv__, AES.MODE_CBC)
+    # pad bytes
+    ct_bytes = Padding.pad(image_bytes, 16)
     cyphertext = CFB_cypher.encrypt(ct_bytes)
 
     return cyphertext
@@ -54,35 +54,35 @@ def CBC_Encrypt(image_bytes):
 
 def CFB_Encrypt(image_bytes):
     """
-    AES -CFB Encryption Mode 
+    AES -CFB Encryption Mode
 
     @param image_bytes Bytes to encrypt
 
     @return Return image bytes as ciphertext
     """
-    
-    #set cipher mode
-    CFB_cypher =  AES.new(__hexkey__, AES.MODE_CFB,segment_size=8,iv=__iv__)
-    #encrypt bytes
+
+    # set cipher mode
+    CFB_cypher = AES.new(__hexkey__, AES.MODE_CFB, segment_size=8, iv=__iv__)
+    # encrypt bytes
     ciphertext = CFB_cypher.encrypt(image_bytes)
     return ciphertext
 
 
-#Ask user for input
+# Ask user for input
 print("Enter Path to bmp image")
 image_name = input()
 
 try:
-    #Check file extention
+    # Check file extention
     img_name_split = image_name.split(".")
     if img_name_split[1] != "bmp":
         raise Exception
-#catch error
+# catch error
 except Exception:
     print("File must be BMP")
     exit()
-    
-#get image bytes 
+
+# get image bytes
 try:
     image = Image.open(image_name)
     image_bytes = image.tobytes()
@@ -90,22 +90,17 @@ except FileNotFoundError:
     print(image_name + " File not found")
     exit()
 
-#Encrypt image using ECB encryption
+# Encrypt image using ECB encryption
 cipher_text = ECB_Encrypt(image_bytes)
-new_image = Image.frombytes(mode='RGB',size =image.size,data=cipher_text)
-new_image.save(fp="ECB.jpg",format="JPEG")
+new_image = Image.frombytes(mode="RGB", size=image.size, data=cipher_text)
+new_image.save(fp="ECB.jpg", format="JPEG")
 
-#Encrypt image using CFB encryption
+# Encrypt image using CFB encryption
 cipher_text = CFB_Encrypt(image_bytes)
-new_image = Image.frombytes(mode='RGB',size =image.size,data=cipher_text)
-new_image.save(fp="CFB.jpg",format="JPEG")
+new_image = Image.frombytes(mode="RGB", size=image.size, data=cipher_text)
+new_image.save(fp="CFB.jpg", format="JPEG")
 
-#Encrypt image using CBC encryption
+# Encrypt image using CBC encryption
 cipher_text = CBC_Encrypt(image_bytes)
-new_image = Image.frombytes(mode='RGB',size =image.size,data=cipher_text)
-new_image.save(fp="CBC.jpg",format="JPEG")
-
-
-
-
-
+new_image = Image.frombytes(mode="RGB", size=image.size, data=cipher_text)
+new_image.save(fp="CBC.jpg", format="JPEG")
